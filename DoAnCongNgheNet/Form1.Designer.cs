935e523ae684559020e5faf104f3ceaf3ad5064f
phanchicuong <phancuong030@gmail.com>
2023-10-03 08:29:17 +0700

    Login

---
+++ DoAnCongNgheNet/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace DoAnCongNgheNet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lb_username = new System.Windows.Forms.Label();
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.lb_password = new System.Windows.Forms.Label();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.btn_dangnhap = new System.Windows.Forms.Button();
+            this.btn_thoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(188, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(203, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ĐĂNG NHẬP";
+            // 
+            // lb_username
+            // 
+            this.lb_username.AutoSize = true;
+            this.lb_username.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_username.Location = new System.Drawing.Point(67, 139);
+            this.lb_username.Name = "lb_username";
+            this.lb_username.Size = new System.Drawing.Size(168, 27);
+            this.lb_username.TabIndex = 2;
+            this.lb_username.Text = "Tên Tài Khoản :";
+            // 
+            // txt_username
+            // 
+            this.txt_username.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_username.Location = new System.Drawing.Point(251, 131);
+            this.txt_username.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(191, 35);
+            this.txt_username.TabIndex = 3;
+            // 
+            // lb_password
+            // 
+            this.lb_password.AutoSize = true;
+            this.lb_password.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_password.Location = new System.Drawing.Point(114, 205);
+            this.lb_password.Name = "lb_password";
+            this.lb_password.Size = new System.Drawing.Size(121, 27);
+            this.lb_password.TabIndex = 4;
+            this.lb_password.Text = "Mật Khẩu :";
+            // 
+            // txt_password
+            // 
+            this.txt_password.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_password.Location = new System.Drawing.Point(251, 197);
+            this.txt_password.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.PasswordChar = '*';
+            this.txt_password.Size = new System.Drawing.Size(191, 35);
+            this.txt_password.TabIndex = 5;
+            // 
+            // btn_dangnhap
+            // 
+            this.btn_dangnhap.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_dangnhap.Location = new System.Drawing.Point(119, 271);
+            this.btn_dangnhap.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_dangnhap.Name = "btn_dangnhap";
+            this.btn_dangnhap.Size = new System.Drawing.Size(153, 66);
+            this.btn_dangnhap.TabIndex = 6;
+            this.btn_dangnhap.Text = "Đăng Nhập";
+            this.btn_dangnhap.UseVisualStyleBackColor = true;
+            // 
+            // btn_thoat
+            // 
+            this.btn_thoat.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_thoat.Location = new System.Drawing.Point(302, 271);
+            this.btn_thoat.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_thoat.Name = "btn_thoat";
+            this.btn_thoat.Size = new System.Drawing.Size(151, 66);
+            this.btn_thoat.TabIndex = 7;
+            this.btn_thoat.Text = "Thoát";
+            this.btn_thoat.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 542);
+            this.Controls.Add(this.btn_thoat);
+            this.Controls.Add(this.btn_dangnhap);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.lb_password);
+            this.Controls.Add(this.txt_username);
+            this.Controls.Add(this.lb_username);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lb_username;
+        private System.Windows.Forms.TextBox txt_username;
+        private System.Windows.Forms.Label lb_password;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.Button btn_dangnhap;
+        private System.Windows.Forms.Button btn_thoat;
+    }
+}
+
